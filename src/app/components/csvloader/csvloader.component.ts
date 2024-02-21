7858f97afc6f83b322a509789faa24347c4627d9
JACOB <j.carballido@gmail.com>
2024-02-21 17:17:13 +0100

    added colors and clip

--- src/app/components/csvloader/csvloader.component.ts
+++ src/app/components/csvloader/csvloader.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PlotlyModule } from 'angular-plotly.js';
 import moment from 'moment';
 import * as PlotlyJS from 'plotly.js-dist-min';
+import { max, mean, min, std, sum } from "mathjs";
+import { isNumber } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
@@ -20,13 +22,19 @@ export class CsvloaderComponent {
   orientation=1;
   simulatedSpeed_kmh = 2;
   dataObject :any[] = [];
+  clipYmm:number=99999;
+
 
   public updateSpeed(val:any)
   {
     this.simulatedSpeed_kmh = val.target.value;
     if(!this.useFileSpeed) this.redrawChart();
   }
-
+  public updateClip(val:any)
+  {
+    this.clipYmm = val.target.value;
+    this.redrawChart();
+  }
   public async loadFile(file:any)
   {
     if(file.files && file.files[0])
@@ -46,6 +54,7 @@ export class CsvloaderComponent {
     let headers = [];
     let propertyCntMax = 0;
     let propertyCnt = 0;
+    this.dataObject = [];
     
     let dataPoint : any = {};
 
@@ -57,9 +66,6 @@ export class CsvloaderComponent {
           fillingHeaders = false;
           propertyCntMax++;
         }else{
-          //line.push(field);
-          //lines.push(line);
-          //line = [];
           dataPoint[headers[propertyCnt++]]=field;
           propertyCnt = 0;
           this.dataObject.push(dataPoint);
@@ -70,19 +76,17 @@ export class CsvloaderComponent {
       }
       if(text[index] == ";" || text[index] == ",")
       {
-          // if(field.indexOf("T#")>-1)
-          // {
-          //   //field =  moment(field, "T#HHMMSSMS").toString();
-          //   distance += 100;
-          //   line.push(distance);
-          // }
           if(fillingHeaders)
           {
             headers.push(field)
             propertyCntMax++;
           }else{
-            //line.push(field);
-            dataPoint[headers[propertyCnt++]]=field;
+            if(!isNaN(Number(field)))
+            {dataPoint[headers[propertyCnt++]]=field;
+            } else{
+              dataPoint[headers[propertyCnt++]]="-";
+            }
+
           }
           field="";
           continue;
@@ -97,8 +101,11 @@ export class CsvloaderComponent {
       ////////////////
       for(let i=1; i<12; i++)
       {
-        dataPoint["Lidar_mm_x_"+i] = dataPoint["Lidar_mm_"+i] * Math.cos((i-6)*8*Math.PI/180);
-        dataPoint["Lidar_mm_y_"+i] = dataPoint["Lidar_mm_"+i] * Math.sin((i-6)*8*Math.PI/180); 
+        if(dataPoint["Lidar_mm_"+i] != 65535 && dataPoint["Lidar_mm_"+i] != "-")
+        {
+          dataPoint["Lidar_mm_x_"+i] = dataPoint["Lidar_mm_"+i] * Math.cos((i-6)*8*Math.PI/180);
+          dataPoint["Lidar_mm_y_"+i] = dataPoint["Lidar_mm_"+i] * Math.sin((i-6)*8*Math.PI/180); 
+        }
       }
     })    
 
@@ -115,44 +122,65 @@ export class CsvloaderComponent {
     if(this.useLines) markers = 'lines+markers';
 
     //add all x points
-    let points_lidar :any =   {x:[],y:[],z:[],power:[]};
-    let points_baumer :any =  {x:[],y:[],z:[],power:[]};
-    let points_ifm :any =     {x:[],y:[],z:[],power:[]};
-    let points_sick :any =    {x:[],y:[],z:[],power:[]};
+    let points_lidar :any =   {x:[],y:[],z:[],power:[],colorRaw:[]};
+    let points_baumer :any =  {x:[],y:[],z:[],power:[],colorRaw:[]};
+    let points_ifm :any =     {x:[],y:[],z:[],power:[],colorRaw:[]};
+    let points_sick :any =    {x:[],y:[],z:[],power:[],colorRaw:[]};
     let points_sensorsPos :any =    {x:[],y:[],z:[],power:[]};
 
     let distance_mm = 0;
-
     
     this.dataObject.forEach((dataPoint,idx)=>{
 
       ////////////////
       //add lidar   
       ////////////////
+
+     
+
+      let idxZ = 0;
       for(let i=1; i<12; i++)
       {       
-        points_lidar.x.push(distance_mm);
+        if(dataPoint["Lidar_mm_y_"+i] == undefined || dataPoint["Lidar_mm_y_"+i] == undefined)continue;
+
+        if( Number(dataPoint["Lidar_mm_y_"+i]) > this.clipYmm ||
+            Number(dataPoint["Lidar_mm_x_"+i]) > this.clipYmm ||
+            Number(dataPoint["Lidar_mm_y_"+i]) < -this.clipYmm ||
+            Number(dataPoint["Lidar_mm_x_"+i]) < -this.clipYmm  )
+        {
+          continue;
+        }      
+       
+        points_lidar.x.push(distance_mm);      
+
         switch(this.orientation)
         {
           case 1:
             points_lidar.y.push(dataPoint["Lidar_mm_y_"+i]);
             points_lidar.z.push(-dataPoint["Lidar_mm_x_"+i]);
+            points_lidar.colorRaw.push( -dataPoint["Lidar_mm_x_"+i]);
             break;
 
           case 2:
             points_lidar.y.push(-dataPoint["Lidar_mm_x_"+i]);
-            points_lidar.z.push(-dataPoint["Lidar_mm_y_"+i]);
+            points_lidar.z.push(-dataPoint["Lidar_mm_y_"+i]);            
+            points_lidar.colorRaw.push(-dataPoint["Lidar_mm_x_"+i]);
             break;
 
           case 3:
             points_lidar.y.push(dataPoint["Lidar_mm_x_"+i]);
             points_lidar.z.push(dataPoint["Lidar_mm_y_"+i]);
+            points_lidar.colorRaw.push( dataPoint["Lidar_mm_y_"+i]);
             break;
         }      
-       
-        points_lidar.power.push(dataPoint["Lidar_Echo_" + i]);
-      }
 
+        
+        
+        points_lidar.power.push(dataPoint["Lidar_Echo_" + i]);
+
+      }
+      
+      
       //add Baumer
       points_baumer.x.push(distance_mm);
      
@@ -187,6 +215,16 @@ export class CsvloaderComponent {
       //add IFM
       for(let i=0; i<10; i++)
       {
+        if(dataPoint["IFM_X_mm_"+i] == "-" || dataPoint["IFM_Y_mm_"+i] == "-") continue;
+
+        if( Number(dataPoint["IFM_Y_mm_"+i]) > this.clipYmm ||
+            Number(dataPoint["IFM_X_mm_"+i]) > this.clipYmm ||
+            Number(dataPoint["IFM_Y_mm_"+i]) < -this.clipYmm ||
+            Number(dataPoint["IFM_X_mm_"+i]) < -this.clipYmm  )
+        {
+          continue;
+        }     
+
         //points_ifm.x.push(dataPoint["IFM_Z_mm_"+i] + distance_mm);
         points_ifm.x.push(distance_mm);
         
@@ -196,16 +234,19 @@ export class CsvloaderComponent {
           case 1:
             points_ifm.y.push(dataPoint["IFM_Y_mm_"+i]);
             points_ifm.z.push(-dataPoint["IFM_X_mm_"+i]);
+            points_ifm.colorRaw.push(-dataPoint["IFM_X_mm_"+i]);
             break;
 
           case 2:
             points_ifm.z.push(-dataPoint["IFM_Y_mm_"+i]);
             points_ifm.y.push(-dataPoint["IFM_X_mm_"+i]);
+            points_ifm.colorRaw.push(-dataPoint["IFM_X_mm_"+i]);
             break;
 
           case 3:
             points_ifm.z.push(dataPoint["IFM_Y_mm_"+i]);
             points_ifm.y.push(dataPoint["IFM_X_mm_"+i]);
+            points_ifm.colorRaw.push(dataPoint["IFM_X_mm_"+i]);
             break;
         }      
       }
@@ -219,28 +260,70 @@ export class CsvloaderComponent {
 
       if(this.useFileSpeed)
       {
-        distance_mm += dataPoint["iSpeed_mm_s"] * 0.1;
+        distance_mm += Number(dataPoint["iSpeed_mm_s"]) * 0.1;
       }else{
         distance_mm += this.simulatedSpeed_kmh*1000/3.6 * 0.1;
       }
     })    
 
 
+    ////////////////////
+    /// LIDAR COLOR CAL
+    ////////////////////
+
+    let maxValue = -999999;
+    let minValue = 999999;
+    points_lidar.color = [];
+    points_lidar.colorRaw.forEach((y:any,indx:number)=>
+    {
+      if (y!=0 && y>maxValue)maxValue = y;
+      if (y!=0 && y<minValue)minValue = y;
+    })
+    points_lidar.colorRaw.forEach((y:any,indx:number)=>
+      {
+        points_lidar.color.push(this.getColorScaled(y,maxValue,minValue));
+      }
+    )
+
+    /////////////////
+    /// IFM COLOR CAL
+    /////////////////
+
+    maxValue = -999999;
+    minValue = 999999;
+    points_ifm.color = [];
+    points_ifm.colorRaw.forEach((y:any,indx:number)=>
+    {
+      if (y!=0 && y>maxValue)maxValue = y;
+      if (y!=0 && y<minValue)minValue = y;
+    })
+    points_ifm.colorRaw.forEach((y:any,indx:number)=>
+      {
+        points_ifm.color.push(this.getColorScaled(y,maxValue,minValue));
+      }
+    )
+
     this.graph.data = [];
-
-
+    let type2 = "surface";
+    let type = "scatter3d";
     let dataLidar = {
-      x:points_lidar.x, y: points_lidar.y, z: points_lidar.z,
+      x: points_lidar.x, 
+      y: points_lidar.y, 
+      z: points_lidar.z,
+      //color : points_lidar.elements,
+      //color_discrete_sequence : points_lidar.color,
+      //surfaceaxis: 2,
+      //surfacecolor: 'rgba(0,255,0,0.5)',
       mode: markers,
       name:'Lidar',
       marker: {
         size: 5,
         // line: {
-        // color: 'rgba(217, 217, 217, 0.8)',
+        color: points_lidar.color,
         // width: 0.2},
         opacity: 0.9},
       //color: points_lidar.power,
-      type: 'scatter3d'
+      type: type
     };
     this.graph.data.push(dataLidar);
 
@@ -255,9 +338,9 @@ export class CsvloaderComponent {
         // color: 'rgba(217, 217, 217, 0.8)',
         // width: 0.2},
         opacity: 0.9},
-      type: 'scatter3d'
+      type: type
     };
-    this.graph.data.push(dataBaumer);
+    //this.graph.data.push(dataBaumer);
 
     let dataIfm = {
       x:points_ifm.x, y: points_ifm.y, z: points_ifm.z,
@@ -266,10 +349,10 @@ export class CsvloaderComponent {
       marker: {
         size: 5,
         // line: {
-        // color: 'rgba(217, 217, 217, 0.8)',
+          color: points_ifm.color,
         // width: 0.2},
         opacity: 0.9},
-      type: 'scatter3d'
+      type: type
     };
     this.graph.data.push(dataIfm);
 
@@ -283,7 +366,7 @@ export class CsvloaderComponent {
         // color: 'rgba(217, 217, 217, 0.8)',
         // width: 0.2},
         opacity: 0.9},
-      type: 'scatter3d'
+      type: type
     };
     this.graph.data.push(sensor);
   }
@@ -297,6 +380,60 @@ export class CsvloaderComponent {
   }
 
   public graph : any = {
-    data:[]
+    data:[],
+    layout: {height: 800}
   };
+
+  private getColorScaled(val:number,valMax:number,valMin:number)
+  {
+    let value = (val - valMin) / (valMax - valMin);
+    const initColor = this.hexToRgb('#eb4034');
+    const centerColor = this.hexToRgb('#f0ec1d');
+    const endColor = this.hexToRgb('#51f211');
+    return this.findColorBetween(value, initColor, centerColor, endColor);
+  }
+
+  hexToRgb(hex: string): any {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  findColorBetween(percentage: number, left: rgb, midle: rgb, right: rgb): string {
+    if (percentage < 0) percentage = 0;
+    var newColor = { r: 0, g: 0, b: 0, a: 255 };
+    var init = { r: 0, g: 0, b: 0 };
+    var end = { r: 0, g: 0, b: 0 };
+    if (percentage < 0.5) {
+      init = left;
+      end = midle;
+      percentage *= 2;
+    }
+    else {
+      init = midle;
+      end = right;
+      percentage = (percentage - 0.5) * 2;
+    }
+    newColor.r = Math.round(init.r + (end.r - init.r) * percentage);
+    newColor.g = Math.round(init.g + (end.g - init.g) * percentage);
+    newColor.b = Math.round(init.b + (end.b - init.b) * percentage);
+    return 'rgba('+ newColor.r +','+ newColor.g +','+ newColor.b +',1)';
+  }
+
+  isNumeric(str:any) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(Number(str)) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
+}
+export interface rgb {
+  r: number, g: number, b: number
 }
